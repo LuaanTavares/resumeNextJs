063ed678a2a0a207fa59f2896c8875fe21ee0226
Luan Tavares <luankriok18@gmail.com>
2023-05-01 10:53:06 -0300

    fix

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,8 +1,8 @@
-import React from "react";
+// import React from "react";
 
-export const Dashboard = () =>{
-  return(
-    <>
-    </>
-  )
-}
+// export const Dashboard = () =>{
+//   return(
+//     <>
+//     </>
+//   )
+// }
